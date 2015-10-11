afaa9d9c047863ba39e8708a3c573732f35fe6e5
Brandon Holt <bholt@cs.washington.edu>
2015-10-10 18:41:52 -0700

    working hello-world for redis

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+extern crate redis;
+use redis::Commands;
+use redis::{RedisResult, Connection};
+
+fn check(c: &Connection) -> RedisResult<isize> {
+    let _ : () = try!(c.set("foo", 42));
+    c.get("foo") as RedisResult<isize>
+}
+
 fn main() {
-    println!("Hello, world!");
+    // connect to redis
+    let client = redis::Client::open("redis://127.0.0.1/").unwrap();
+    let c = client.get_connection().unwrap();
+    
+    println!("check: {}", check(&c).unwrap());
+    println!("success!");
 }
