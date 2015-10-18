52266255bc99d2185fd17282c27b93e3f21cd6bd
Brandon Holt <bholt@cs.washington.edu>
2015-10-17 17:10:11 -0700

    playing with how to handle various Retwis entities

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,61 @@ extern crate redis;
 use redis::Commands;
 use redis::{RedisResult, Connection};
 
+macro_rules! argmap(
+    { $($key:expr => $value:expr),+ } => { &[ $( ($key, &*$value) ),+ ] };
+);
+
 fn check(c: &Connection) -> RedisResult<isize> {
-    let _ : () = try!(c.set("foo", 42));
+    try!(c.set("foo", 42));
     c.get("foo") as RedisResult<isize>
+}
+
+struct BetterClient<'a> { conn: &'a Connection }
+
+impl<'a> BetterClient<'a> {
+    fn set<T: Entity>(&self, e: &T) -> RedisResult<()> {
+        e.set(&self.conn)
+    }
+}
+
+trait Entity {
+    fn key(i64) -> String;
+    fn set(&self, c: &Connection) -> RedisResult<()>;
+}
+
+struct User {
+    id: i64,
+    username: String,
+    password: String,
+    first: String,
+    last: String
+}
+
+impl User {
+    fn new(id: i64, username: &str, password: &str, first: &str, last: &str) -> User {
+        User {
+            id: id,
+            username: username.to_string(),
+            password: password.to_string(),
+            first: first.to_string(),
+            last: last.to_string()
+        }
+    }
+}
+
+impl Entity for User {
+    
+    fn key(id: i64) -> String { id.to_string() + ":user" }
+    
+    fn set(&self, c: &Connection) -> RedisResult<()> {
+        c.hset_multiple(Self::key(self.id), argmap!(
+            "username" => self.username,
+            "password" => self.password,
+            "first"    => self.first,
+            "last"     => self.last
+        ))
+    }
+
 }
 
 fn main() {
@@ -13,5 +65,18 @@ fn main() {
     let c = client.get_connection().unwrap();
     
     println!("check: {}", check(&c).unwrap());
-    println!("success!");
+    
+    let u = User::new(1, "bholt", ".", "Brandon", "Holt");
+    
+    println!("user.key => {}", User::key(u.id));
+
+    {    
+        let b = BetterClient{ conn: &c };
+        b.set(&u).unwrap();
+    }
+        
+    let r : Vec<String> = c.hget(User::key(u.id), &["username","first","last"]).unwrap();
+    println!("check user: {:?}", r);
+    
+    println!("done");
 }
